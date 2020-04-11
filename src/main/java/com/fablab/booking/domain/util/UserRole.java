55f8fbab9698529d02e16748ad84549e2d4aacb7
Vlad <vladbantus02@gmail.com>
2020-04-11 23:48:54 +0300

    added user and userAuthority domain classes

---
+++ src/main/java/com/fablab/booking/domain/util/UserRole.java
@@ -0,0 +1,9 @@
+package com.fablab.booking.domain.util;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ROLE_USER, ROLE_ADMIN
+
+}
---
+++ src/main/java/com/fablab/booking/repository/UserAuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.fablab.booking.repository;
+
+import com.fablab.booking.domain.UserAuthority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAuthorityRepository extends JpaRepository<UserAuthority, Long> {
+}
