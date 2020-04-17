d2f634d6575d1a81d07f6f86481325c8dfb8767b
Vlad <vladbantus02@gmail.com>
2020-04-17 19:08:25 +0300

    disabled cors

---
+++ src/main/java/com/fablab/booking/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.fablab.booking.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    //TODO change Cors config
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
