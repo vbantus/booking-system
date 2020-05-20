ac57f07e3dd40f477cacd16597df046ceb670031
Vladdd97 <vladbantus02@gmail.com>
2020-05-21 00:00:38 +0300

    added comment-user relation

--- src/main/java/com/fablab/booking/dto/RqCreateCommentDto.java
+++ src/main/java/com/fablab/booking/dto/RqCreateCommentDto.java
@@ -1,10 +1,12 @@
 package com.fablab.booking.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class RqCreateCommentDto {
     private String content;
     private Long articleId;
+    @ApiModelProperty(value = "userId", example = "1")
     private Long userId;
 }
--- src/main/java/com/fablab/booking/dto/RsCommentDto.java
+++ src/main/java/com/fablab/booking/dto/RsCommentDto.java
@@ -6,5 +6,5 @@ import lombok.Data;
 public class RsCommentDto extends AbstractResponseEntityDto {
     private String content;
     private Long articleId;
-    private Long userId;
+    private String postedBy;
 }
