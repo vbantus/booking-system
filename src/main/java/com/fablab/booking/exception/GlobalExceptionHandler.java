e3aa0cbc17adcaa9d7d8d876c60f8c4690136fd7
Vladdd97 <vladbantus02@gmail.com>
2020-05-31 16:49:01 +0300

    refactored GlobalExceptionHandler

--- src/main/java/com/fablab/booking/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fablab/booking/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.fablab.booking.exception;
 
-import com.google.common.collect.Iterables;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -8,21 +8,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({EntityNotFoundException.class})
-    public ResponseEntity<String> handleNotFoundExceptions(RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
-
-    @ExceptionHandler({TimeConflictException.class, BookingNotAllowedException.class})
-    public ResponseEntity<String> handleTimeConflictExceptions(RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
-    }
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
@@ -40,13 +30,28 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
-    @ExceptionHandler({ConstraintViolationException.class})
-    public ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException ex) {
-
-        List<String> errors = ex.getConstraintViolations().stream()
-                .map(e -> Iterables.getLast(e.getPropertyPath()).getName() + ": " + e.getMessage())
-                .collect(Collectors.toList());
-
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errors);
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<String> handleNotFoundExceptions(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
+
+    @ExceptionHandler({TimeConflictException.class, BookingNotAllowedException.class})
+    public ResponseEntity<String> handleTimeConflictExceptions(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getSQLException().getMessage());
+    }
+
+    //    @ExceptionHandler({ConstraintViolationException.class})
+//    public ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException ex) {
+//
+//        List<String> errors = ex.getConstraintViolations().stream()
+//                .map(e -> Iterables.getLast(e.getPropertyPath()).getName() + ": " + e.getMessage())
+//                .collect(Collectors.toList());
+//
+//        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errors);
+//    }
 }
