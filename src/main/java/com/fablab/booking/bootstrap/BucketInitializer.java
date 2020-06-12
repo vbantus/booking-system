f99d3c1cfe0042002821d43804eae788a5b9ea49
Vlad <vladbantus02@gmail.com>
2020-06-12 17:12:20 +0300

    added room bucket creation

--- src/main/java/com/fablab/booking/bootstrap/BucketInitializer.java
+++ src/main/java/com/fablab/booking/bootstrap/BucketInitializer.java
@@ -16,10 +16,13 @@ public class BucketInitializer implements CommandLineRunner {
     @Value("${minio.buckek.event.name}")
     private String eventBucket;
 
+    @Value("${minio.buckek.room.name}")
+    private String roomBucket;
+
     private final MinioService minioService;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         if (!minioService.bucketExist(articleBucket)) {
             createBucketWithReadOnlyPolicy(articleBucket);
@@ -27,6 +30,10 @@ public class BucketInitializer implements CommandLineRunner {
 
         if (!minioService.bucketExist(eventBucket)) {
             createBucketWithReadOnlyPolicy(eventBucket);
+        }
+
+        if (!minioService.bucketExist(roomBucket)) {
+            createBucketWithReadOnlyPolicy(roomBucket);
         }
     }
 
