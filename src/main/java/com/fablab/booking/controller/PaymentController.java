2dad0b007648296e1692912481396cac3e943428
Vladdd97 <vladbantus02@gmail.com>
2020-06-14 18:23:28 +0300

    added RequestBody annotation to RqPaymentDto

--- src/main/java/com/fablab/booking/controller/PaymentController.java
+++ src/main/java/com/fablab/booking/controller/PaymentController.java
@@ -6,6 +6,7 @@ import com.fablab.booking.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class PaymentController {
     private final EventService eventService;
 
     @PostMapping
-    public ResponseEntity<RsPaymentDto> performPayment(@Valid RqPaymentDto rqPaymentDto) {
+    public ResponseEntity<RsPaymentDto> performPayment(@Valid @RequestBody RqPaymentDto rqPaymentDto) {
         RsPaymentDto rsPaymentDto = RsPaymentDto.builder()
                 .email("booking@gmail.com")
                 .orderId(new Random().nextLong())
